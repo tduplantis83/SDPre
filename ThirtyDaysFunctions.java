8fb30846f0656dc09081ebead6dfe8b0d672db3e
tduplantis83 <tduplantis83@gmail.com>
2019-08-14 09:45:08 -0600

    Exercise 38

---
+++ ThirtyDaysFunctions.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ThirtyDaysFunctions
+{
+  public static void main(String[] args)
+  {
+    Scanner kb = new Scanner(System.in);
+
+    System.out.print("Which month? (1-12) ");
+    int month = kb.nextInt();
+  }
+}
